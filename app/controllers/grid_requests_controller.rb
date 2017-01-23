bd6f15cd9afbc3cadc309462275029cb4f446dc3
Aubrey Goodman <aubrey.goodman@gmail.com>
2017-01-22 17:50:04 -0800

    disable csrf for grid requests

--- app/controllers/grid_requests_controller.rb
+++ app/controllers/grid_requests_controller.rb
@@ -3,6 +3,8 @@ class GridRequestsController < ApplicationController
   include Serviceable
   
   acts_as_service :grid_request
+
+  skip_before_filter :verify_authenticity_token
   
   def assign_new_instance
     @instance = GridRequest.find_or_create_by(min_lat: params[:min_lat], min_lng: params[:min_lng], max_lat: params[:max_lat], max_lng: params[:max_lng])
